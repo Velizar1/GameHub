d41b0bfaed30b1c3e690264fa3da6d1459528ec2
v.d.vrubchev <v.d.vrubchev@gmail.com>
2020-11-16 15:57:25 +0200

    init project

---
+++ src/main/java/gamehub/demo/GamehubApplication.java
@@ -0,0 +1,14 @@
+package gamehub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamehubApplication {
+
+    //org.springframework.validation.BindingResult.userAddBindingModel
+    public static void main(String[] args) {
+        SpringApplication.run(GamehubApplication.class, args);
+    }
+
+}
